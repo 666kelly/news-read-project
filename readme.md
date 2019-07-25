924008df3e520839e4b0dc2cd4b30a9eb8ca4198
666kelly <18621020695@163.com>
2019-07-25 15:16:36 +0800

    do home page

--- readme.md
+++ readme.md
@@ -1,2 +1,21 @@
-##手机商品 
-##cccc
+##制作首页App组件
+1 完成Header区域，使用的mint-ui中的header组件
+2 制作底部的Tabber区域，使用的是mui的Tabber.html
+  *在只做购物车图标麻烦
+  *先把扩展图标的css样式拷贝到项目
+  *然后把.tff文件拷贝到项目中
+  *为购物车添加如下样式
+3 在中间区域放置一个router-view展示路由匹配的组件
+##改造Tabber为router-link
+
+##设置路由高亮
+
+##点击tabber中的路由连接，展示对应的路由组件
+
+##加载首页轮播图数据
+1获取数据用,使用view-resource,
+
+2使用vue-resource的this.$http.get 获取数据
+3获取到的数据要保存到data身上
+4使用v-for循环渲染到每个item上
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 import Vue from 'vue'
+//导入路由的包
+import VueRouter from 'vue-router'
+//安装路由
+Vue.use(VueRouter)
+//导入mint
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(Mint)
-import {Header} from'mint-ui'
+
+import {Header,Swipe, SwipeItem} from'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//2.1导入vue-resource
+import VueResource from 'vue-resource'
+//2.2 安装 vue-resource
+Vue.use(VueResource)
+//导入mui样式
 import './lib/mui/css/mui.min.css'
+import "./lib/mui/css/icons-extra.css"
+
+//导入路由模块
+import router from './router.js'
+
 import app from './App.vue'
 var vm=new Vue({
    el:'#app',
-   render: c =>c(app)
+   render: c =>c(app),
+   router//挂在路由到实例上
 })
